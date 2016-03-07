e5e0cc5a6b5f519a79ea3c711842985644981892
Patrick Schoeps <pschoeps@umich.edu>
2016-03-07 11:55:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodStella::Application.config.secret_key_base = 'b7524e494fe8d487ac390c8d07cfce14d6067cc09892a02a955f0c702309a962ba9aa7a7bfdfac8148675183294c2dae539493e6e528c372f77dd2169ea07393'
