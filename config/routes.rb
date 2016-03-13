42d2097323d49e77bbe940eab18b7ab38e94be7a
Patrick Schoeps <pschoeps@umich.edu>
2016-03-13 14:28:23 -0400

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ FoodStella::Application.routes.draw do
   devise_for :users
   get "static_pages/home"
   root 'static_pages#home'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
