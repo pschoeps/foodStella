495b4545f2cbd9e6ef0e1148a4d6f752a529f208
slvtrs <salulos@gmail.com>
2016-03-26 14:35:49 -0400

    reworked my sloppy height styling to use @layout nicely

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,6 +12,13 @@ class RegistrationsController < Devise::RegistrationsController
   def edit
 
    end
+
+   private
+   def determine_layout
+     #set a lyout variable for determining weather or not to set a layout in the application.html file.  If this variable is set to 
+     #false (in individual controller instances), then the navbar and footer will not be displayed.
+     @layout = false
+   end
    
   
      protected
