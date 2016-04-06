7db062941589e35f8c7b03a767a67664a15debe1
Patrick Schoeps <pschoeps@umich.edu>
2016-04-05 22:39:24 -0400

    fix carrierwave issues

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_bucket = ENV['S3_BUCKET']
   config.aws_acl    = 'public-read'
 
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:            ENV.fetch('AWS_REGION') # Required
+    access_key_id:     ENV['S3_ACCESS_KEY'],
+    secret_access_key: ENV['S3_SECRET_KEY'],
+    region:            ENV['S3_REGION']# Required
   }
 
   end
