8a4a2fa987e7a7bd550f0698d795636173c76163
slvtrs <salulos@gmail.com>
2016-04-11 23:49:22 -0400

    profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,7 @@
 class ProfilesController < ApplicationController
 
 	def new
-		# if current_user.provider == 'facebook'
-			# create prefilled form from user's fb info
-		# else
-			@profile = current_user.build_profile
-		# end
+		@profile = current_user.build_profile
 	end
 
 	def create
@@ -21,26 +17,37 @@ class ProfilesController < ApplicationController
 	end
 
 	def edit
-	  @profile = Profile.find_by_user_id(params[:id])
+		@profile = Profile.find(params[:id])
+	end
+
+	def update
+	  @profile = Profile.find(params[:id])
+	  @profile.update_attributes(profile_params)
+	    respond_to do |format|
+	      format.html  { redirect_to profile_path(current_user) }
+	      flash[:success] =  "Profile Updated"
+	  end
 	end
 
 	def index
-		unless current_user.profile
-			redirect_to new_profile_path
-		end
-		@profile = current_user.profile
+		# @profile = Profile.find_by_user_id(params[:user_id])
+		# @profile = Profile.find(params[:id])
+		# @profiles = Profile.all
 	end
 
 	def show
+		if !current_user.profile && params[:id].to_i == current_user.id
+			redirect_to new_profile_path and return
+		end
+
 		@profile = Profile.find_by_user_id(params[:id])
-		@friends = profile.user.friends
 	end
 
 	def destroy
 	end
 
 	def profile_params
-	  	params.require(:profile).permit(:fir_name, :las_name, :email, :about_me, :image_url, :country, :cooking_experience, :average_cook_time )
+	  	params.require(:profile).permit(:fir_name, :las_name, :email, :about_me, :picture_url, :country, :cooking_experience, :average_cook_time )
 	end
 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,12 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	# for favorite ingredients?
 	# has_many :ingredients
+
+	mount_uploader :picture_url, ProfilePictureUploader
+
+	 def picture_size
+      if picture_url.size > 5.megabytes
+        errors.add(:picture_url, "should be less than 5MB")
+      end
+    end
 end
