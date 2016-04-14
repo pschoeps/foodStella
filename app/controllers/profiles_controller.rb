e2a63d2e567f8b738fd7b4373093b53a6a9720b1
slvtrs <salulos@gmail.com>
2016-04-14 16:17:08 -0400

    profile partials

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,12 +5,10 @@ class ProfilesController < ApplicationController
 	end
 
 	def create
-
 		# if !profile_params[:picture_url] && current_user.image
 			# profile_params[:picture_url] = current_user.image
 			#OR# @profile.update_attribute(:picture_url, current_user.image)
 		# end
-
 		@profile = current_user.build_profile(profile_params)
 
 		if @profile.save
@@ -50,13 +48,20 @@ class ProfilesController < ApplicationController
 		end
 
 		@profile = Profile.find_by_user_id(params[:id])
+		if(params.has_key?(:tab))
+			@tab = params[:tab]
+		else
+			@tab = 'about_me'
+		end
+
+		@users = User.all
 	end
 
 	def destroy
 	end
 
 	def profile_params
-	  	params.require(:profile).permit(:fir_name, :las_name, :email, :about_me, :picture_url, :country, :cooking_experience, :average_cook_time )
+	  	params.require(:profile).permit(:fir_name, :las_name, :email, :about_me, :picture_url, :country, :cooking_experience, :average_cook_time, :tab )
 	end
 
 end
