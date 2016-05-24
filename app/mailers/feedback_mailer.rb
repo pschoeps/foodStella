c21ba4ea12b412b35218ba1a76423255f798da9c
slvtrs <salulos@gmail.com>
2016-05-24 16:59:02 -0400

    mailer fix

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -3,6 +3,6 @@ class FeedbackMailer < ActionMailer::Base
 	def message_me(msg)
 		@msg = msg
 
-		mail from: @msg.email, subject: 'Feedback - ' + @msg.subject, body: 'Feedback from: ' + @msg.name + ' - ' + @msg.email + ' Content: ' + @msg.content
+		mail from: @msg.email, subject: 'Feedback - ' + @msg.subject, body: 'Feedback from: ' + @msg.name + ' -- Address: ' + @msg.email + ' -- Content: ' + @msg.content
 	end
 end
