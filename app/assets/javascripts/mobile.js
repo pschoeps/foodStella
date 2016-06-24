b775862b508624aa43976957eab0c2432ff40064
slvtrs <salulos@gmail.com>
2016-06-24 15:40:11 -0400

    pc

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -357,3 +357,14 @@ $(document).ready(function() {
     };
 
 });
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+    return 'iOS';
+  else if( userAgent.match( /Android/i ) )
+    return 'Android';
+  else
+    return 'unknown';
+}
