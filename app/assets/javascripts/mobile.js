627855c32ae658e0d71e89bb767979e35ca4161f
slvtrs <salulos@gmail.com>
2016-06-27 12:26:50 -0400

    mobile recipe view tweaks

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -356,15 +356,6 @@ $(document).ready(function() {
       $('.meals').css('height', tallest)
     };
 
+
+
 });
-
-function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
-    return 'iOS';
-  else if( userAgent.match( /Android/i ) )
-    return 'Android';
-  else
-    return 'unknown';
-}
