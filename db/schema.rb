1f705a681497fe839cc4edf637672533bd28708b
Haemin Park <haeminp@umich.edu>
2016-12-15 00:02:12 -0500

    changing config to set up

--- db/schema.rb
+++ db/schema.rb
@@ -17,23 +17,23 @@ ActiveRecord::Schema.define(version: 20160918171147) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
+    t.string   "rateable_type"
     t.integer  "rateable_id"
-    t.string   "rateable_type", limit: 255
-    t.float    "avg",                       null: false
+    t.float    "avg",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "commontator_comments", force: :cascade do |t|
-    t.string   "creator_type",      limit: 255
+    t.string   "creator_type"
     t.integer  "creator_id"
-    t.string   "editor_type",       limit: 255
+    t.string   "editor_type"
     t.integer  "editor_id"
-    t.integer  "thread_id",                                 null: false
-    t.text     "body",                                      null: false
+    t.integer  "thread_id",                     null: false
+    t.text     "body",                          null: false
     t.datetime "deleted_at"
-    t.integer  "cached_votes_up",               default: 0
-    t.integer  "cached_votes_down",             default: 0
+    t.integer  "cached_votes_up",   default: 0
+    t.integer  "cached_votes_down", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down", using: :btree
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "commontator_subscriptions", force: :cascade do |t|
-    t.string   "subscriber_type", limit: 255, null: false
-    t.integer  "subscriber_id",               null: false
-    t.integer  "thread_id",                   null: false
+    t.string   "subscriber_type", null: false
+    t.integer  "subscriber_id",   null: false
+    t.integer  "thread_id",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["subscriber_id", "subscriber_type", "thread_id"], name: "index_commontator_subscriptions_on_s_id_and_s_type_and_t_id", unique: true, using: :btree
@@ -53,10 +53,10 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "commontator_threads", force: :cascade do |t|
-    t.string   "commontable_type", limit: 255
+    t.string   "commontable_type"
     t.integer  "commontable_id"
     t.datetime "closed_at"
-    t.string   "closer_type",      limit: 255
+    t.string   "closer_type"
     t.integer  "closer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   create_table "deferred_ingredients", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "ingredient_id"
-    t.string   "ingredient_name", limit: 255
+    t.string   "ingredient_name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["user_id"], name: "index_deferred_ingredients_on_user_id", using: :btree
@@ -85,11 +85,11 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "recipe_id"
-    t.string   "start_at",    limit: 255
-    t.string   "end_at",      limit: 255
+    t.string   "start_at"
+    t.string   "end_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "recipe_name", limit: 255
+    t.string   "recipe_name"
     t.integer  "servings"
     t.index ["recipe_id"], name: "index_events_on_recipe_id", using: :btree
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
@@ -98,17 +98,17 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
-    t.string   "status",      limit: 255
+    t.string   "status"
     t.datetime "created_at"
     t.datetime "accepted_at"
     t.datetime "updated_at"
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "abbreviated", limit: 255
+    t.string   "abbreviated"
     t.decimal  "calories"
     t.string   "category"
   end
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "others_photos", force: :cascade do |t|
-    t.string  "photo_url", limit: 255
+    t.string  "photo_url"
     t.integer "recipe_id"
     t.integer "user_id"
     t.index ["recipe_id"], name: "index_others_photos_on_recipe_id", using: :btree
@@ -131,9 +131,9 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "overall_averages", force: :cascade do |t|
+    t.string   "rateable_type"
     t.integer  "rateable_id"
-    t.string   "rateable_type", limit: 255
-    t.float    "overall_avg",               null: false
+    t.float    "overall_avg",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -141,19 +141,19 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   create_table "preferred_ingredients", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "ingredient_id"
-    t.string   "ingredient_name", limit: 255
+    t.string   "ingredient_name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["user_id"], name: "index_preferred_ingredients_on_user_id", using: :btree
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.string   "fir_name",             limit: 255
-    t.string   "las_name",             limit: 255
-    t.string   "email",                limit: 255
-    t.string   "country",              limit: 255
+    t.string   "fir_name"
+    t.string   "las_name"
+    t.string   "email"
+    t.string   "country"
     t.text     "about_me"
-    t.string   "picture_url",          limit: 255
+    t.string   "picture_url"
     t.integer  "cooking_experience"
     t.integer  "average_cook_time"
     t.datetime "created_at"
@@ -161,23 +161,23 @@ ActiveRecord::Schema.define(version: 20160918171147) do
     t.integer  "user_id"
     t.text     "liked_foods"
     t.text     "disliked_foods"
-    t.string   "username",             limit: 255
+    t.string   "username"
     t.text     "cookware_preferences"
-    t.string   "background_url",       limit: 255
+    t.string   "background_url"
     t.integer  "background_offset"
     t.boolean  "show_full_name"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
   create_table "quantities", force: :cascade do |t|
-    t.string   "amount",        limit: 255
-    t.string   "decimal",       limit: 255
-    t.string   "ingredient",    limit: 255
+    t.string   "amount"
+    t.string   "decimal"
+    t.string   "ingredient"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "recipe_id"
     t.integer  "ingredient_id"
-    t.string   "unit",          limit: 255
+    t.string   "unit"
     t.text     "detail"
     t.decimal  "ounces"
     t.index ["ingredient_id"], name: "index_quantities_on_ingredient_id", using: :btree
@@ -186,10 +186,10 @@ ActiveRecord::Schema.define(version: 20160918171147) do
 
   create_table "rates", force: :cascade do |t|
     t.integer  "rater_id"
+    t.string   "rateable_type"
     t.integer  "rateable_id"
-    t.string   "rateable_type", limit: 255
-    t.float    "stars",                     null: false
-    t.string   "dimension",     limit: 255
+    t.float    "stars",         null: false
+    t.string   "dimension"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
@@ -197,11 +197,11 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "rating_caches", force: :cascade do |t|
+    t.string   "cacheable_type"
     t.integer  "cacheable_id"
-    t.string   "cacheable_type", limit: 255
-    t.float    "avg",                        null: false
-    t.integer  "qty",                        null: false
-    t.string   "dimension",      limit: 255
+    t.float    "avg",            null: false
+    t.integer  "qty",            null: false
+    t.string   "dimension"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
@@ -209,20 +209,20 @@ ActiveRecord::Schema.define(version: 20160918171147) do
 
   create_table "recipes", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "name",             limit: 255
-    t.string   "photo_url",        limit: 255
-    t.string   "category",         limit: 255
+    t.string   "name"
+    t.string   "photo_url"
+    t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
     t.integer  "prep_time"
     t.integer  "cook_time"
-    t.string   "difficulty",       limit: 255
-    t.string   "meal_type",        limit: 255
+    t.string   "difficulty"
+    t.string   "meal_type"
     t.integer  "servings"
-    t.string   "website_url",      limit: 255
+    t.string   "website_url"
     t.text     "cookware"
-    t.string   "remote_photo_url", limit: 255
+    t.string   "remote_photo_url"
     t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
   end
 
@@ -246,33 +246,33 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                       limit: 255, default: "", null: false
-    t.string   "encrypted_password",          limit: 255, default: "", null: false
-    t.string   "reset_password_token",        limit: 255
+    t.string   "email",                       default: "", null: false
+    t.string   "encrypted_password",          default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                           default: 0,  null: false
+    t.integer  "sign_in_count",               default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
-    t.string   "provider",                    limit: 255
-    t.string   "uid",                         limit: 255
-    t.string   "image",                       limit: 255
-    t.string   "name",                        limit: 255
-    t.string   "fir_name",                    limit: 255
-    t.string   "las_name",                    limit: 255
-    t.string   "location",                    limit: 255
-    t.string   "about_me",                    limit: 255
-    t.string   "hometown",                    limit: 255
-    t.string   "country",                     limit: 255
-    t.integer  "day_counter",                             default: 3
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
+    t.string   "name"
+    t.string   "fir_name"
+    t.string   "las_name"
+    t.string   "location"
+    t.string   "about_me"
+    t.string   "hometown"
+    t.string   "country"
+    t.integer  "day_counter",                 default: 3
     t.datetime "day_counter_last_updated_at"
-    t.string   "username",                    limit: 255
-    t.string   "age_range",                   limit: 255
-    t.string   "confirmation_token",          limit: 255
+    t.string   "username"
+    t.string   "age_range"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.datetime "birthday"
@@ -282,12 +282,12 @@ ActiveRecord::Schema.define(version: 20160918171147) do
   end
 
   create_table "votes", force: :cascade do |t|
+    t.string   "votable_type"
     t.integer  "votable_id"
-    t.string   "votable_type", limit: 255
+    t.string   "voter_type"
     t.integer  "voter_id"
-    t.string   "voter_type",   limit: 255
     t.boolean  "vote_flag"
-    t.string   "vote_scope",   limit: 255
+    t.string   "vote_scope"
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
